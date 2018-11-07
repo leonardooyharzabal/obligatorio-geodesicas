6ece3982576f47804f893654777406021cb6ee27
Leonardo Oyharzabal <leonardo.oyharzabal@mercadolibre.com>
2018-11-06 21:56:16 -0300

    cambio plot resultado rk4

--- rk4/script_run_method.m
+++ rk4/script_run_method.m
@@ -9,8 +9,8 @@
 ##dq/dt = pq (-2u/1+v^2+u^2)
 
 ## with the Runge-Kutta method (implemented in rk4.m).
-[T,X]=rk4('g',[0,10],[1;0.5;0.52721;0.5],0.01); # added timestep parameter, dt=0.01
-plot(X(:,1),X(:,2),X(:,3),X(:,4))
+[T,X]=rk4('g',[0,10],[1;0.5;0.5;0.5],0.01); # added timestep parameter, dt=0.01
+plot(T,X);
 pause(60);
 
 
